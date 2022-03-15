bf3ad3df4b36f5e61267894f170b7d9e3528531d
slopez <s.lopezreyes255@gmail.com>
2022-03-15 15:28:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Example project using symfony 6, php 8.1 and mysql8, all dockerized (including git and composer)
---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+];
